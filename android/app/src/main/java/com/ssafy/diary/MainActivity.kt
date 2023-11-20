64e148f30c1889c798107f0894e061742420abdc
sehyun <germs1020@gmail.com>
2023-11-20 17:33:16 +0900

    feat: bottomNavigation 추가, 프래그먼트 이동시 애니메이션 추가

--- android/app/src/main/java/com/ssafy/diary/MainActivity.kt
+++ android/app/src/main/java/com/ssafy/diary/MainActivity.kt
@@ -10,10 +10,12 @@ import com.ssafy.diary.login.FindIdFragment
 import com.ssafy.diary.login.JoinFragment
 import com.ssafy.diary.login.LoginFragment
 import com.ssafy.diary.login.OpenFragment
+import com.ssafy.diary.nav.MainFragment
 import com.ssafy.diary.util.SharedPreferencesUtil
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var status = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -21,9 +23,20 @@ class MainActivity : AppCompatActivity() {
         // 로그아웃 하지 않아 sharedPreference에 회원 정보가 저장되어 있으면 바로 서비스 연결
         val userInfo = SharedPreferencesUtil(this).getUser()
         if(userInfo.userId != ""){
-            moveFragment(DIARY_MAIN_FRAGMENT)
+            moveFragment(MAIN_FRAGMENT)
         } else{
-            moveFragment(OPEN_FRAGMENT)
+            moveFragment(LOGIN_FRAGMENT)
+        }
+
+        // 하단 네비게이션 바
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.action_one -> moveFragment(MAIN_FRAGMENT)
+                R.id.action_two -> moveFragment(DIARY_MAIN_FRAGMENT)
+                R.id.action_three -> moveFragment(OPEN_FRAGMENT)
+                else -> OPEN_FRAGMENT
+            }
+            true
         }
 
     }
@@ -32,24 +45,32 @@ class MainActivity : AppCompatActivity() {
     fun moveFragment(destination: Int){
         when(destination){
             0 -> supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fadein, R.anim.fadeout, R.anim.fadein, R.anim.fadeout)
                 .replace(R.id.fragment_container, OpenFragment())
                 .addToBackStack(null)
                 .commit()
             1 -> supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fadein, R.anim.fadeout, R.anim.fadein, R.anim.fadeout)
                 .replace(R.id.fragment_container, LoginFragment())
                 .addToBackStack(null)
                 .commit()
             2 -> supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fadein, R.anim.fadeout, R.anim.fadein, R.anim.fadeout)
                 .replace(R.id.fragment_container, JoinFragment())
                 .addToBackStack(null)
                 .commit()
             3 -> supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fadein, R.anim.fadeout, R.anim.fadein, R.anim.fadeout)
                 .replace(R.id.fragment_container, FindIdFragment())
                 .addToBackStack(null)
                 .commit()
             4 -> supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fadein, R.anim.fadeout, R.anim.fadein, R.anim.fadeout)
+                .replace(R.id.fragment_container, MainFragment())
+                .commit()
+            5 -> supportFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.fadein, R.anim.fadeout, R.anim.fadein, R.anim.fadeout)
                 .replace(R.id.fragment_container, DiaryMainFragment())
-                .addToBackStack(null)
                 .commit()
         }
 
@@ -62,11 +83,15 @@ class MainActivity : AppCompatActivity() {
             .commit()
         supportFragmentManager.popBackStack()
     }
+
+
     companion object{
         const val OPEN_FRAGMENT = 0
         const val LOGIN_FRAGMENT = 1
         const val JOIN_FRAGMENT = 2
         const val FIND_ID_FRAGMENT = 3
-        const val DIARY_MAIN_FRAGMENT = 4
+        const val MAIN_FRAGMENT = 4
+        const val DIARY_MAIN_FRAGMENT = 5
     }
+
 }
--- android/app/src/main/java/com/ssafy/diary/nav/MainFragment.kt
+++ android/app/src/main/java/com/ssafy/diary/nav/MainFragment.kt
@@ -1,14 +1,20 @@
 package com.ssafy.diary.nav
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ssafy.diary.R
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.ssafy.diary.MainActivity
+import com.ssafy.diary.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    private val binding by lazy { FragmentMainBinding.inflate(layoutInflater) }
+    private val mActivity by lazy { activity as MainActivity }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +26,12 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        Glide.with(this)
+            .load(com.ssafy.diary.R.drawable.character02)
+            .apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
+            .into(binding.imgMainItem01)
+        return binding.root
     }
 
 }
