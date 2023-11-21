2e91b5a0f6374b49c689bd5bbcbfdf1b1ec01729
sehyun <germs1020@gmail.com>
2023-11-21 16:21:16 +0900

    feat: 인벤토리 조회 구현

---
+++ android/app/src/main/java/com/ssafy/diary/dto/InventoryItem.kt
@@ -0,0 +1,8 @@
+package com.ssafy.diary.dto
+
+data class InventoryItem(
+    var userId: String,
+    var itemId: Int,
+    var itemName: String,
+    var itemType: String
+)
--- android/app/src/main/java/com/ssafy/diary/nav/TodoListFragment.kt
+++ android/app/src/main/java/com/ssafy/diary/nav/TodoListFragment.kt
@@ -1,6 +1,7 @@
 package com.ssafy.diary.nav
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -50,7 +51,9 @@ class TodoListFragment : Fragment() {
             } else{
                 binding.checkboxTodayDiary.setBackgroundResource(R.drawable.check_box_style)
             }
-            
+
+            val inventoryItems = RetrofitUtil.inventoryService.getInventory(userId).body()
+            Log.d("해위", inventoryItems.toString())
 
         }
 
